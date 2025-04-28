47c4d51acc12c659ff86de0740115d7cb54c39ae
carlitos <carlos.viur.cv@gmail.com>
2025-04-27 22:41:27 -0400

    Screens creadas y barril actualizado

---
+++ lib/presentation/screens/infinite_scroll/infinite_scroll_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class InfiniteScrollScreen extends StatelessWidget {
+  static const name = 'Infinite screen';
+
+  const InfiniteScrollScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
